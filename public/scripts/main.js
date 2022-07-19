ec0d234a97fb974c1f4608810aa0135c7a3c0f78
rhit-deckerza <deckerza@rose-hulman.edu>
2022-07-19 12:45:32 -0700

    made more good progress on the home page

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,12 +1,42 @@
-/**
- * @fileoverview
- * Provides the JavaScript interactions for all pages.
- *
- * @author 
- * PUT_YOUR_NAME_HERE
- */
+const { element } = require('prop-types');
+const React = require('react')
+const ReactDOM = require('react-dom')
 
-/** namespace. */
-var rhit = rhit || {};
+function weekDay(id, weekDay, date){
+        return (
+            <span id={id}>
+                <span>
+                    {weekDay}
+                </span>
+                <span>
+                    {date}
+                </span>
+                <span class="myLine">
+                    
+                </span>
+            </span>
+        );
+        }
 
-/** globals */
+class cal extends React.Component{
+    constructor(props){
+        super(props);
+        this.weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        this.date = getDate();
+        this.weekDay = this.weekDays[getDay()];
+    }
+
+    render(){
+        <div id="cal">
+            {weekDay("oneCal", this.weekDay, this.date)};
+        </div>
+    }
+
+}
+
+const root = ReactDOM.createRoot(
+    document.getElementById("root"));
+element = (<div>HIHIHIHIHIHIHIH</div>);
+root.render(element);
+root.appendChild(element);
+print("HI");
